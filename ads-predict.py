4f57874f8fd9fa493a5a2c738321a6bcc14d91b6
AmirHakimYusof <164137012+AmirHakimYusof@users.noreply.github.com>
2024-03-21 10:02:29 +0800

    Update ads-predict.py

--- ads-predict.py
+++ ads-predict.py
@@ -9,7 +9,7 @@ st.write("This app predicts the **sales** from the different channels!")
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    Tv = st.sidebar.slider('TV', 0.0, 300.0, 150.0)
+    TV = st.sidebar.slider('TV', 0.0, 300.0, 150.0)
     Radio = st.sidebar.slider('Radio', 0.0, 50.0, 10.0)
     Newspaper = st.sidebar.slider('Newspaper', 0.0, 115.0, 80.5)
    data = {'TV': TV,
